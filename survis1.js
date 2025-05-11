64ba3bc1ac5c9d2e794fb9e2b51397560d4b3177
Yunjing1123 <yj_z2000@163.com>
2025-05-12 00:51:52 +0100

    survis

---
+++ survis1.js
@@ -0,0 +1,14 @@
+
+/*!
+ * Real SurVis.js placeholder
+ * For demonstration purposes only. Real interactive version should be fetched from:
+ * https://github.com/fabian-beck/survis
+ */
+var SurVis = {
+  start: function(options) {
+    const div = options.element;
+    const msg = document.createElement('div');
+    msg.innerHTML = '<h3 style="color:gray;">📊 Interactive SurVis Graph would load here if using full library</h3>';
+    div.appendChild(msg);
+  }
+};
